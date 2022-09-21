3c873f73de3857fe4ffd0059a2f3473670817936
SdmfDovi <dovi.yarmush@gmail.com>
2022-09-21 20:33:48 +0300

    add print line

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class MyTest {
+    @Test
+    public void test1(){
+        System.out.println("Change 1");
+
+        System.out.println("2");
+
+    }
+}
+
